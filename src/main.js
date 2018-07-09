dfb5e2a5a811a02974b095307b313e7ccd7031c5
Jennifer <jlawrence010@gmail.com>
2018-07-09 15:24:55 -0700

    trilio set up and working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+// import mongoose from 'mongoose';
+
 import { startServer } from './lib/server';
 
+
+import Message from './model/message';
+
+const myMessage = new Message({
+  accountId: 'potato',
+  sentTo: '+16502079775',
+  body: 'sup world',
+  reminderId: 'potatoID',
+  replyTime: null,
+  repyBody: null,
+});
+
+console.log(myMessage);
 startServer();
+myMessage.sendText();
