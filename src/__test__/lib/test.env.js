b7072cf689bacdbab8458dae54e4586f1dff4071
Jennifer <jlawrence010@gmail.com>
2018-07-10 09:28:22 -0700

    edited test.env.js to remove twilio info

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -2,6 +2,3 @@ process.env.NODE_ENV = 'development';
 process.env.PORT = 5000;
 process.env.MONGODB_URI = 'mongodb://localhost/testing';
 process.env.SECRET_KEY = 'helloWorldkdsjflad';
-process.env.TWILIO_ACCOUNT_SID = 'ACa95d84e8c74b9854d72577673a672dc0';
-process.env.TWILIO_AUTH_TOKEN = '8fbc67e36846a289e6ae813678e55801';
-process.env.TWILIO_NUMBER = '+14155790418';
